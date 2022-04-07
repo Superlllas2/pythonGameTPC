a45369a412eb9c3469a93b1b737fda68c58f4adb
Superlllas2 <stenakirpichnaya@mail.ru>
2022-04-07 22:52:30 +0200

    Added first window logic, but it does not fully work. Saving just to save a backup

--- FirstWindow.py
+++ FirstWindow.py
@@ -3,29 +3,26 @@ from pygame.locals import *
 import os
 from PIL import Image
 
-class StartUpWindow():
-    dirname = os.path.dirname(__file__)
 
-    # pygame.init()
-    # flags = pygame.OPENGL
-    # DISPLAYSURF = pygame.display.set_mode((400, 300), flags, vsync=1)
-    # pygame.display.set_caption('The Game')
+class StartUpWindow():
+    # dirname = os.path.dirname(__file__)
+
+    pygame.init()
+    flags = pygame.OPENGL
+    DISPLAYSURF = pygame.display.set_mode((400, 300), flags, vsync=1)
+    pygame.display.set_caption('The Game')
     #
     # WHITE = (255, 255, 255)
     # catImg = pygame.image.load((os.path.join(dirname, 'Resources/Pictures/Background/ToiletMan.png')))
-    Image.open(os.path.join(dirname, 'Resources/Pictures/Background/ToiletMan.png'))
+    # Image.open(os.path.join(dirname, 'Resources/Pictures/Background/ToiletMan.png'))
     # catx = 10
     # caty = 10
     # direction = 'right'
 
 
-
-
-
-
-    # while True:
-    #     for event in pygame.event.get():
-    #         if event.type == QUIT:
-    #             pygame.quit()
-    #             sys.exit()
-    #     pygame.display.set_mode()
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+        pygame.display.set_mode()
